ff95a74a9f1e57673b90c7528b3b93a00e75c523
Bex Tuychiev <bex@ibexprogramming.com>
2022-12-12 14:52:07 +0500

    feature: Create preprocess.py to preprocess images with different techniques

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,5 +1,37 @@
 from joblib import Parallel, delayed
-from skimage import imread
+from skimage.io import imread, imsave
 from skimage.transform import resize
+from pathlib import Path
+from tqdm import tqdm
+import warnings
 import numpy as np
 
+warnings.filterwarnings("ignore")
+
+DATA_DIR = Path("data")
+train_dir = DATA_DIR / "raw" / "train"
+
+
+def resize_image(image_path, target_size):
+    """
+    Resize image to target size.
+    """
+    image = imread(image_path)
+    image = resize(image, target_size, anti_aliasing=True)
+
+    # Create the path to the image in the prepared directory
+    target_path = str(image_path).replace("raw", "prepared")
+    Path(target_path).parent.mkdir(parents=True, exist_ok=True)
+
+    imsave(target_path, image, )
+
+
+if __name__ == "__main__":
+    image_paths = []
+
+    for directory in train_dir.iterdir():
+        image_paths.extend(list(directory.glob("*.png")))
+
+    Parallel(n_jobs=10, backend="multiprocessing")(
+        delayed(resize_image)(path, (150, 150)) for path in tqdm(image_paths)
+    )
