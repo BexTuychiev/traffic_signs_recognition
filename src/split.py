67eb54711a4c3af3668e52a12ee6388b1e179ab0
Bex Tuychiev <bex@ibexprogramming.com>
2022-12-18 12:39:33 +0500

    Update the src/split.py to shuffle the image paths before moving them to a test directory

--- src/split.py
+++ src/split.py
@@ -1,27 +1,33 @@
 import shutil
 from pathlib import Path
+import numpy as np
+
+np.random.seed(42)
 
 base_dir = Path(__file__).parent.parent
 raw_train_dir = base_dir / "data" / "raw" / "train"
-raw_validation_dir = base_dir / "data" / "raw" / "validation"
+raw_test_dir = base_dir / "data" / "raw" / "test"
 
 # Copy 10% of train images to validation directory
 for directory in raw_train_dir.iterdir():
-    validation_mirror_path = str(directory).replace("train", "validation")
-    validation_mirror_path = Path(validation_mirror_path)
-    validation_mirror_path.mkdir(parents=True, exist_ok=True)
+    test_mirror_path = str(directory).replace("train", "test")
+    test_mirror_path = Path(test_mirror_path)
+    test_mirror_path.mkdir(parents=True, exist_ok=True)
+
     # Collect image paths in each class of train directory
     image_paths = list(directory.glob("*.png"))
-    # Choose the last 10% of images
-    validation_images = image_paths[-int(len(image_paths) * 0.1):]
+    np.random.shuffle(image_paths)
+
+    # Choose 10% of images
+    test_images = image_paths[-int(len(image_paths) * 0.1):]
 
     # Copy images to validation directory
-    for image_path in validation_images:
-        shutil.move(image_path, validation_mirror_path)
+    for image_path in test_images:
+        shutil.move(image_path, test_mirror_path)
 
 # # Reverse the above operation
-# for directory in raw_validation_dir.iterdir():
-#     train_mirror_path = str(directory).replace("validation", "train")
+# for directory in raw_test_dir.iterdir():
+#     train_mirror_path = str(directory).replace("test", "train")
 #     train_mirror_path = Path(train_mirror_path)
 #
 #     # Collect image paths in each class of validation directory
